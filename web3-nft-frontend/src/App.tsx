a4079a9e66ac40ca744cef914bad8bcfb83ca6e0
lmw <alsdndjdrktl@gmail.com>
2022-03-26 16:29:25 +0900

    feat : contracts 연결 및 metamask 연동

--- web3-nft-frontend/src/App.tsx
+++ web3-nft-frontend/src/App.tsx
@@ -1,10 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './routes/main';
 
 const App: FC = () => {
+ const [account, setAccount] = useState<string>('');
+ const getAccount = async () => {
+  try {
+   if (window.ethereum) {
+    const accounts = await window.ethereum.request({
+     method: 'eth_requestAccounts',
+    });
+    setAccount(accounts[0]);
+   } else {
+    alert('Install Metamast!');
+   }
+  } catch (err) {
+   console.error(err);
+  }
+ };
+
+ useEffect(() => {
+  getAccount();
+ }, []);
  return (
   <BrowserRouter>
+   <div>{account}</div>
    <Routes>
     <Route path='/' element={<Main />} />
    </Routes>
