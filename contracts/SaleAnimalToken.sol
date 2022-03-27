4b6c2f249b59a372b611c60bd967a570983095e5
lmw <alsdndjdrktl@gmail.com>
2022-03-27 14:38:00 +0900

    feat : 오타 수정 및 animal 가격 가져오기

--- contracts/SaleAnimalToken.sol
+++ contracts/SaleAnimalToken.sol
@@ -2,54 +2,58 @@
 
 pragma solidity ^0.8.0;
 
- import "MintAnimalToken.sol";
+import "MintAnimalToken.sol";
 
- contract SaleAnimalToken {
-     MintAnimalToken public mintAnimalTokenAddress;
+contract SaleAnimalToken {
+    MintAnimalToken public mintAnimalTokenAddress;
 
-     constructor (address _mintAnimalTokenAddress) {
-         mintAnimalTokenAddress = MintAnimalToken(_mintAnimalTokenAddress);
-     }
+    constructor (address _mintAnimalTokenAddress) {
+        mintAnimalTokenAddress = MintAnimalToken(_mintAnimalTokenAddress);
+    }
 
-     mapping(uint256 => uint256) public animalTokenPrices;
+    mapping(uint256 => uint256) public animalTokenPrices;
 
-     uint256[] public onSaleAnimalTokenArray;
+    uint256[] public onSaleAnimalTokenArray;
 
-     function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
-         address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+    function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
 
-         require(animalTokenOwner == msg.sender, "Caller is not animal token owner.");
-         require(_price > 0, "Price is zero or lower.");
-         require(animalTokenPrices[_animalTokenId] == 0, "This animal token is already on sale.");
-         require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)), "Animal token owner did not approve token.");
+        require(animalTokenOwner == msg.sender, "Caller is not animal token owner.");
+        require(_price > 0, "Price is zero or lower.");
+        require(animalTokenPrices[_animalTokenId] == 0, "This animal token is already on sale.");
+        require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)), "Animal token owner did not approve token.");
 
-         animalTokenPrices[_animalTokenId] = _price;
+        animalTokenPrices[_animalTokenId] = _price;
 
-         onSaleAnimalTokenArray.push(_animalTokenId);
-     }
+        onSaleAnimalTokenArray.push(_animalTokenId);
+    }
 
-     function purchaseAnimalToken(uint256 _animalTokenId) public payable {
-         uint256 price = animalTokenPrices[_animalTokenId];
-         address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
-         require(price > 0, "Animal token not sale.");
-         require(price <= msg.value, "Caller sen't lower than price.");
-         require(animalTokenOwner != msg.sender, "Caller is animal token owner.");
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = animalTokenPrices[_animalTokenId];
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
 
-         payable(animalTokenOwner).transfer(msg.value);
-         mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
+        require(price > 0, "Animal token not sale.");
+        require(price <= msg.value, "Caller sent lower than price.");
+        require(animalTokenOwner != msg.sender, "Caller is animal token owner.");
 
-         animalTokenPrices[_animalTokenId] = 0;
+        payable(animalTokenOwner).transfer(msg.value);
+        mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
 
-         for (uint256 i = 0; i < onSaleAnimalTokenArray.length; i++) {
-             if (animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
-                 onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
-                 onSaleAnimalTokenArray.pop();
-             }
-         }
-         
-     }
+        animalTokenPrices[_animalTokenId] = 0;
 
-     function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
-         return onSaleAnimalTokenArray.length;
-     }
- }
+        for(uint256 i = 0; i < onSaleAnimalTokenArray.length; i++) {
+            if(animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
+                onSaleAnimalTokenArray.pop();
+            }
+        }
+    }
+
+    function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
+        return onSaleAnimalTokenArray.length;
+    }
+
+    function getAnimalTokenPrice(uint256 _animalTokenId) view public returns (uint256) {
+        return animalTokenPrices[_animalTokenId];
+    }
+}
