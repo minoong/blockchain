48cc864130c10f726659d49391db85e4d8d8115c
lmw <alsdndjdrktl@gmail.com>
2022-03-27 15:18:58 +0900

    feat : 의존관계 세팅 및 NTF 구조체 및 배열 반환

--- contracts/MintAnimalToken.sol
+++ contracts/MintAnimalToken.sol
@@ -3,9 +3,18 @@
  pragma solidity ^0.8.0;
 
  import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+ import "SaleAnimalToken.sol";
 
  contract MintAnimalToken is ERC721Enumerable {
      constructor() ERC721("h662Animals", "HAS") {}
+
+     SaleAnimalToken public saleAnimalToken;
+
+     struct AnimalTokenData {
+         uint256 animalTokenId;
+         uint256 animalType;
+         uint256 animalPrice;
+     }
 
      mapping(uint256 => uint256) public animalTypes;
 
@@ -17,5 +26,27 @@
          animalTypes[animalTokenId] = animalType;
 
          _mint(msg.sender, animalTokenId);
+     }
+
+     function getAnimalTokens(address _animalTokenOwner) view public returns (AnimalTokenData[] memory) {
+         uint256 balanceLength = balanceOf(_animalTokenOwner);
+
+         require(balanceLength != 0, "Owner did not have token.");
+
+         AnimalTokenData[] memory animalTokenData = new AnimalTokenData[](balanceLength);
+
+         for (uint256 i = 0; i < balanceLength; i++) {
+             uint256 animalTokenId = tokenOfOwnerByIndex(_animalTokenOwner, i);
+             uint256 animalType = animalTypes[animalTokenId];
+             uint256 animalPrice = saleAnimalToken.getAnimalTokenPrice(animalTokenId);
+
+             animalTokenData[i] = AnimalTokenData(animalTokenId, animalType, animalPrice);
+         }
+
+         return animalTokenData;
+     }
+
+     function setSaleAnimalToken(address _saleAnimalToken) public {
+         saleAnimalToken = SaleAnimalToken(_saleAnimalToken);
      }
  }
